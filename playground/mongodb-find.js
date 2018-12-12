b58a9fb6cc2c14a99ddec1bf51d27587e2110fc9
Pacius <timothy.pacius@accenture.com>
2018-12-11 21:19:35 -0500

    Init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,34 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+    const db = client.db('TodoApp');
+
+    // db.collection('Todos').find({
+    //     _id: ObjectID('5c105216229cfc05d04f2db2')
+    // }).toArray().then((docs) => {
+    //     console.log('Todos');
+    //     console.log(JSON.stringify(docs, undefined, 2));
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // });
+
+    // db.collection('Todos').find().count().then((count) => {
+    //     console.log(`Todos count: ${count}`);
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // });
+
+    db.collection('Users').find({name: 'Timothy'}).count().then((count) => {
+        console.log(`User count: ${count}`);
+    }, (err) => {
+        console.log('Unable to fetch todos', err);
+    });
+
+
+    // client.close();
+});

7db40e92a365f3f28ad5fb32c3ff0896daae5a72
Pacius <timothy.pacius@accenture.com>
2018-12-11 21:45:44 -0500

    Added delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,35 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+    const db = client.db('TodoApp');
+
+    //deleteMany
+    // db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result) => {
+    //     console.log(result);
+    // });
+
+    //deleteOne
+    // db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result) => {
+    //     console.log(result);
+    // });
+
+    //findOneAndDelete
+    // db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+    //     console.log(result);
+    // });
+
+    db.collection('Users').deleteMany({name: 'Timothy'}).then((result) => {
+        console.log(result);
+    });
+
+    db.collection('Users').findOneAndDelete({_id: ObjectID('5c1057ac67e54c48e4aa4949')}).then((result) => {
+        console.log(result);
+    });
+
+    // client.close();
+});

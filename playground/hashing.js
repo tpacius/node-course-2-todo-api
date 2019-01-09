345aa72e0e6c017662fcdcb69e26d60a17116c4e
Timothy Pacius <tpacius@bu.edu>
2019-01-09 17:20:05 -0500

    Hashes user passwords before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,20 +1,35 @@
 const { SHA256 } = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10
-};
+let password = '123abc';
 
-let token = jwt.sign(data, '123abc');
+// let salt = bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash);
+//     });
+// });
 
-console.log(token);
+let hashed = '$2a$10$r5bxSdmNMmG/E4TNQMnlCu/YDB2sLiBrHRkDaLLWAqMUJjD3bow0q';
 
-let decoded = jwt.verify(token, '123abc');
-console.log(decoded);
+bcrypt.compare(password, hashed, (err, res) => {
+    console.log(res);
+});
 
-let message = 'fill me in';
-let hash = SHA256(message).toString();
+// var data = {
+//     id: 10
+// };
 
-console.log(`Message: ${message}`);
-console.log(`Hash: ${hash}`);
+// let token = jwt.sign(data, '123abc');
+
+// console.log(token);
+
+// let decoded = jwt.verify(token, '123abc');
+// console.log(decoded);
+
+// let message = 'fill me in';
+// let hash = SHA256(message).toString();
+
+// console.log(`Message: ${message}`);
+// console.log(`Hash: ${hash}`);
 

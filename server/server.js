086d86eaf3779bab7d07f81ef22d2f6567b9714f
Timothy Pacius <tpacius@bu.edu>
2019-01-14 13:03:03 -0500

    Add POST /user/login route

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectId} = require('mongodb');
+const bcrypt = require('bcryptjs');
 
 let {mongoose} = require('./db/mongoose');
 let {Todo} = require('./models/todo');
@@ -109,6 +110,17 @@ app.post('/users', (req, res) => {
 
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
+});
+
+app.post('/users/login', (req, res) => {
+   let body = _.pick(req.body, ['email', 'password']);
+   User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+        res.header('x-auth', token).send(user);
+    });
+   }).catch((e) => {
+        res.status(400).send();
+   });
 });
 
 app.listen(port, () => {

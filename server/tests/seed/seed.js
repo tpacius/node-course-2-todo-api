8fdd435536db90235f6a50f926239dba425652e9
Timothy Pacius <tpacius@bu.edu>
2019-01-15 17:34:47 -0500

    Updated all todos methods and tests to authenicate users

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -18,17 +18,23 @@ const users = [{
 }, {
     _id: userTwoId,
     email: 'mit@testscript.com',
-    password: 'userTwo'
+    password: 'userTwo',
+    tokens: [{
+        access: 'auth',
+        token: jwt.sign({_id: userTwoId, access: 'auth'}, 'abc123').toString()
+    }]
 }];
 
 const todos = [{
     _id: new ObjectId(),
-    text: 'Test todo 1'
+    text: 'Test todo 1',
+    _creator: userOneId
 }, {
     _id: new ObjectId(),
     text: 'Test todo 2',
     completed: true,
-    completedAt: 333
+    completedAt: 333,
+    _creator: userTwoId
 }];
 
 const populateTodos = (done) => {
